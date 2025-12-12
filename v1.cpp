7b726456c482d3e4806483a6a1a9a1de093ab396
Mamun123123123 <almalm90459@gmail.com>
2025-12-12 15:35:37 +0600

    none

--- v1.cpp
+++ v1.cpp
@@ -2,25 +2,6 @@
 using namespace std;
 
 void print(vector<int> data){
-    cout << data.size() << "\n"; 
-    for(int datam : data){
-        cout << datam << " ";
-    }
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    // create vector 
-    // vector<int> data(5); 
-    vector<int> data = {1,2,5};
-    // data.push_back(3);
-    // data.push_back(4);
-    data.insert(data.begin(),8);#include <bits/stdc++.h>
-using namespace std;
-
-void print(vector<int> data){
     cout << data.size() << "\n";
     for(int datam : data){
         cout << datam << " ";
