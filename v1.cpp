0c83b719b9375bdc386dbccffa32c3dd44816f38
Mamun123123123 <almalm90459@gmail.com>
2025-12-12 15:35:00 +0600

    vector creating completed

--- v1.cpp
+++ v1.cpp
@@ -17,7 +17,33 @@ int main(){
     vector<int> data = {1,2,5};
     // data.push_back(3);
     // data.push_back(4);
+    data.insert(data.begin(),8);#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> data){
+    cout << data.size() << "\n";
+    for(int datam : data){
+        cout << datam << " ";
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // create vector
+    // vector<int> data(5);
+    vector<int> data = {1,2,5};
+    // data.push_back(3);
+    // data.push_back(4);
     data.insert(data.begin(),8);
+    data.insert(data.end(),9);
+    data.insert(data.begin() + 2,10);
+    data.insert(data.begin()+1,2,9);
+    print(data);
+}
+
+
     data.insert(data.end(),9);
     print(data);
 }
