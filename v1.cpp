d48e944b3e3e39c9acfee9a3a54486c1a95b8cd0
Mamun123123123 <almalm90459@gmail.com>
2025-12-12 16:02:27 +0600

    vector delete,erase

--- v1.cpp
+++ v1.cpp
@@ -14,13 +14,19 @@ int main(){
 
     // create vector
     // vector<int> data(5);
-    vector<int> data = {1,2,5};
+    //vector<int> data = {1,2,5};
     // data.push_back(3);
     // data.push_back(4);
-    data.insert(data.begin(),8);
-    data.insert(data.end(),9);
-    data.insert(data.begin() + 2,10);
-    data.insert(data.begin()+1,2,9);
-    print(data);
-}
+    //data.insert(data.begin(),8);
+    //data.insert(data.end(),9);
+    //data.insert(data.begin() + 2,10);
+    //data.insert(data.begin()+1,2,9);
+    //print(data);
+    vector<int> data1 = {1,2,3,4,5,6};
+    //for(int dat:data1){
+     //   cout << dat;
+    //}
+        data1.erase(data1.begin()+2 , data1.begin()+3);
+     print(data1);
 
+}
